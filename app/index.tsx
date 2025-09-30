840c1d4ca3c0eb587152a22d58bf721268806dfc
KatieMari <katie.glover@student.norwichuni.ac.uk>
2025-09-30 14:17:02 +0100

    fixed web viewing

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import WhiteKey from "@/Components/WhiteKey";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function Index(){
   return <View style={styles.containter}>
@@ -7,16 +7,6 @@ export default function Index(){
   </View>
 }
 
-
-export default function Index() {
-  return (
-    <View
-      style={styles.containter}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
   containter:{
