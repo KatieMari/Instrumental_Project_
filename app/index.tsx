567121de82eb3f755da283d4e98362f7586bef4e
KatieMari <katie.glover@student.norwichuni.ac.uk>
2025-09-30 15:45:50 +0100

    added piano components

--- app/index.tsx
+++ app/index.tsx
@@ -1,33 +1,14 @@
-import WhiteKey from "@/Components/WhiteKey";
+import Piano from '@/Components/Piano';
 import useOrientation from "@/Hooks/useOrientation";
 import { Orientation } from "expo-screen-orientation";
-import { StyleSheet, Text, View } from "react-native";
-
+import { Text } from "react-native";
 
 export default function Index() {
   const CurrentOrientation = useOrientation();
-  const output = (<View style={styles.containter}>
-    <WhiteKey></WhiteKey>
-    <WhiteKey></WhiteKey>
-    <WhiteKey></WhiteKey>
-    <WhiteKey></WhiteKey>
-    <WhiteKey></WhiteKey>
-    <WhiteKey></WhiteKey>
-    <WhiteKey></WhiteKey>
-  </View>)
-  if (CurrentOrientation === Orientation.LANDSCAPE_LEFT) return output;
-  if (CurrentOrientation === Orientation.LANDSCAPE_RIGHT) return output;
+  
+  if (CurrentOrientation === Orientation.LANDSCAPE_LEFT) return <Piano/>;
+  if (CurrentOrientation === Orientation.LANDSCAPE_RIGHT) return <Piano/>;
   else return <Text>Rotate Device to Landscape</Text>
-
 }
 
 
-const styles = StyleSheet.create({
-  containter: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: '#2592e'
-  }
-})
